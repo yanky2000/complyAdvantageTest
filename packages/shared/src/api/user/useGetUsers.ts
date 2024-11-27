15005e03238b1521577eed363b1916b227908cb2
Victor Yang <yanky2000@yahoo.com>
2024-11-27 16:31:48 +0000

    shared: change user query data from arr to obj

--- packages/shared/src/api/user/useGetUsers.ts
+++ packages/shared/src/api/user/useGetUsers.ts
@@ -1,10 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import fetchTyped from '../../utils/fetchTyped';
-import type { GetUsersResponse } from '../../mockApi/types';
+import type { GetUsersResponse, User } from '../../mockApi/types';
 
 export const useGetUsersQuery = () => {
   return useQuery({
     queryKey: ['users'],
     queryFn: () => fetchTyped<GetUsersResponse>('/api/users', {}),
+    select: (users) => {
+      return users?.reduce(
+        (acc, user) => {
+          acc[user.identifier] = user;
+          return acc;
+        },
+        {} as Record<User['identifier'], User>,
+      );
+    },
   });
 };
