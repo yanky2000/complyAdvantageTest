6991bf5efb1144b92c7d27f8bf238db50bf5d1af
Victor Yang <yanky2000@yahoo.com>
2024-12-01 19:31:56 +0000

    shared: remove dev comment

--- packages/shared/src/api/user/useGetUsers.ts
+++ packages/shared/src/api/user/useGetUsers.ts
@@ -1,19 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import fetchTyped from '../../utils/fetchTyped';
-import type { GetUsersResponse, User } from '../../mockApi/types';
+import type { GetUsersResponse } from '../../mockApi/types';
 
 export const useGetUsersQuery = () => {
   return useQuery({
     queryKey: ['users'],
     queryFn: () => fetchTyped<GetUsersResponse>('/api/users', {}),
-    // select: (users) => {
-    //   return users?.reduce(
-    //     (acc, user) => {
-    //       acc[user.identifier] = user;
-    //       return acc;
-    //     },
-    //     {} as Record<User['identifier'], User>,
-    //   );
-    // },
   });
 };
